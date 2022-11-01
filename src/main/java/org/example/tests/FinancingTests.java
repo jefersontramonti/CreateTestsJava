894b3b1a477a1c90876323f526aa3f9ff44c7946
Jeferson Tramonti <jefersontramonti@outlook.com>
2022-10-31 21:11:25 -0300

    init tests

---
+++ src/main/java/org/example/tests/FinancingTests.java
@@ -0,0 +1,28 @@
+package org.example.tests;
+
+
+import org.example.entities.Financing;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FinancingTests {
+
+    //Deve criar o objeto com os dados corretos quando os dados forem válidos
+    @Test
+    public void constructorShouldCreateObjectWhenValidDate() {
+
+        Financing f = new Financing(100000.0, 2000.0, 80);
+
+        Assertions.assertEquals(100000.0, f.getTotalAmount());
+        Assertions.assertEquals(2000.0, f.getIncome());
+        Assertions.assertEquals(80, f.getMonths());
+    }
+
+    //Deve lançar IllegalArgumentException quando os dados não forem válidos
+    @Test
+    public void constructorShouldThrowIllegalArgumentExceptionWhenInvalidDate(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing f = new Financing(100000.0, 2000.0, 20);
+        });
+    }
+}
